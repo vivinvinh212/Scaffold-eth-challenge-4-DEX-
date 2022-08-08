05c2291d6c9aaee8deb01b9456f59e260c2686b6
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-08-08 13:57:50 +0700

    add new function

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -1,16 +1,42 @@
 pragma solidity >=0.8.0 <0.9.0;
 // SPDX-License-Identifier: MIT
 // import "hardhat/console.sol";
+
+import "LPEthToken.sol"
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DEX {
+    IERC20 token;
+    LPEthToken public lpEthToken; //<---Define token variable
 
-  IERC20 token;
+    //Add this events definitions
+    event EthToTokenSwap(
+        address swapper,
+        string txDetails,
+        uint256 ehtInput,
+        uint256 tokenOutput
+    );
+    event TokenToEthSwap(
+        address swapper,
+        string txDetails,
+        uint256 tokenInput,
+        uint256 ehtOutput
+    );
+    event LiquidityProvided(
+        address liquidityProvider,
+        uint256 tokenInput,
+        uint256 ethInput,
+        uint256 liquidityMinted
+    );
+    event LiquidityRemoved(
+        address liquidityRemover,
+        uint256 tokenOutput,
+        uint256 ethOutput,
+        uint256 liquidityBurned
+    );
 
-  constructor(address token_addr) {
-    token = IERC20(token_addr);
-  }
-
-  // write your functions here...
-
+    constructor(address token_addr) {
+        token = IERC20(token_addr);
+        lpEthToken = new LPEthToken("LPEthToken", "LPDEX"); //<---Deploy contract and set token varible
+    }
 }
--- packages/hardhat/contracts/LPEthToken.sol
+++ packages/hardhat/contracts/LPEthToken.sol
@@ -2,5 +2,18 @@ pragma solidity >=0.8.0 <0.9.0;
 // SPDX-License-Identifier: MIT
 // import "hardhat/console.sol";
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LPEthToken is Ownable, ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
