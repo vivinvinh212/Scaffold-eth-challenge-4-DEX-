94f7b8d25cdb74afec98c05a57e571b0916b2099
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-08-08 13:37:20 +0700

    Create LPEthToken.sol

---
+++ packages/hardhat/contracts/LPEthToken.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.8.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+// import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
